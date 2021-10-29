daa41fd925a74154c17493ba046ce087dc5d4e79
Paweł Stawikowski <stawikowski@zse.pl>
2021-10-29 10:05:46 +0200

    price matchinig

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tinydb import TinyDB, Query
 from bs4 import BeautifulSoup
 from date import format_date
 import requests
+import re
 
 
 db = TinyDB('db.json')
@@ -17,11 +18,25 @@ def add_link():
         else:
             r = requests.get(link).content
             soup = BeautifulSoup(r, 'html.parser')
-            date = soup.find("span", {"data-cy": "ad-posted-at"}).text
-            date = format_date(date)
-            db.insert({'link': link,
-                       'date': date})
-            print(date)
+
+            date = soup.find("span", {"data-cy": "ad-posted-at"})
+
+            if date is not None:
+                date = date.text
+
+                title = soup.find("h1", {"data-cy": "ad_title"}).text
+
+                price_div = soup.find("div", {"data-testid": "ad-price-container"})
+                price = int(''.join(re.findall(r'[0-9]', price_div.find("h3").text)))
+
+                print(price)
+
+                date = format_date(date)
+                db.insert({'link': link,
+                           'title': title,
+                           'price': price,
+                           'date': date})
+                print(date)
 
 
 def check_links():
