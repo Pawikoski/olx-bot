8337caa6d45d136235364687b878429e48e50970
Paweł Stawikowski <stawikowski@zse.pl>
2021-10-29 11:02:54 +0200

    checking if record already exists in db

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ db = TinyDB('db.json')
 def add_link():
     print("Enter anything but no link to exit")
     while True:
-        link = input("Add link:")
+        link = input("Add link: ")
 
         if "http" not in link and "olx.pl" not in link:
             break
@@ -29,14 +29,67 @@ def add_link():
                 price_div = soup.find("div", {"data-testid": "ad-price-container"})
                 price = int(''.join(re.findall(r'[0-9]', price_div.find("h3").text)))
 
-                print(price)
-
                 date = format_date(date)
-                db.insert({'link': link,
-                           'title': title,
-                           'price': price,
-                           'date': date})
-                print(date)
+
+                # views = soup.find("span", {"data-testid": "page-view-text"}).text
+                # print(views)
+
+                auction_data = {
+                    'link': link,
+                    'title': title,
+                    'price': price,
+                    'date': date
+                }
+
+                does_exist(auction_data)
+
+
+def does_exist(auction_data: dict):
+    q = Query()
+    link = str(auction_data['link'])
+    search_result = db.search(q.link == link)
+    if len(search_result) > 0:
+        print("Already exists in database!")
+
+        old_title = search_result[0]['title']
+        old_price = search_result[0]['price']
+        old_date = search_result[0]['date']
+
+        new_title = auction_data['title']
+        new_price = auction_data['price']
+        new_date = auction_data['date']
+
+        new_auction_data = dict()
+        new_auction_data['link']  = link
+
+        if old_title != new_title:
+            print("Title has been changed.")
+            new_auction_data['title'] = new_title
+
+        if old_price != new_price:
+            print("Price has been changed.")
+            new_auction_data['price'] = new_price
+
+        if old_date != new_date:
+            print("Auction has been refreshed")
+            new_auction_data['date'] = new_date
+
+        if len(new_auction_data) > 1:
+            user_choice = input("Some data in auction has been changed. Do you want to update database? (Y/N)")
+            while True:
+                if user_choice.lower() == "n":
+                    print("Database won't be updated")
+                    break
+                elif user_choice.lower() == "y":
+                    print("Database will be updated")
+                    # db change czy cos tutaj
+                    break
+
+    else:
+        db.insert(auction_data)
+
+
+
 
 
 def check_links():
