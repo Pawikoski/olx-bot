1bd8f46537595af3e704d97cff7e8a42e55eae9d
Paweł Stawikowski <stawikowski@zse.pl>
2021-10-29 11:56:19 +0200

    updating db in separate function

--- main.py
+++ main.py
@@ -44,6 +44,43 @@ def add_link():
                 does_exist(auction_data)
 
 
+def update_db(auction_data, search_result):
+    old_title = search_result[0]['title']
+    old_price = search_result[0]['price']
+    old_date = search_result[0]['date']
+
+    new_title = auction_data['title']
+    new_price = auction_data['price']
+    new_date = auction_data['date']
+    link = auction_data['link']
+
+    new_auction_data = dict()
+    new_auction_data['link'] = link
+
+    if old_title != new_title:
+        print("Title has been changed.")
+        new_auction_data['title'] = new_title
+
+    if old_price != new_price:
+        print("Price has been changed.")
+        new_auction_data['price'] = new_price
+
+    if old_date != new_date:
+        print("Auction has been refreshed")
+        new_auction_data['date'] = new_date
+
+    if len(new_auction_data) > 1:
+        user_choice = input("Some data in auction has been changed. Do you want to update database? (Y/N)")
+        while True:
+            if user_choice.lower() == "n":
+                print("Database won't be updated")
+                break
+            elif user_choice.lower() == "y":
+                print("Database will be updated")
+                db.update({'title': new_title, 'price': new_price, 'date': new_date}, where('link') == link)
+                break
+
+
 def does_exist(auction_data: dict):
     q = Query()
     link = str(auction_data['link'])
@@ -51,47 +88,22 @@ def does_exist(auction_data: dict):
     if len(search_result) > 0:
         print("Already exists in database!")
 
-        old_title = search_result[0]['title']
-        old_price = search_result[0]['price']
-        old_date = search_result[0]['date']
-
-        new_title = auction_data['title']
-        new_price = auction_data['price']
-        new_date = auction_data['date']
-
-        new_auction_data = dict()
-        new_auction_data['link'] = link
-
-        if old_title != new_title:
-            print("Title has been changed.")
-            new_auction_data['title'] = new_title
-
-        if old_price != new_price:
-            print("Price has been changed.")
-            new_auction_data['price'] = new_price
-
-        if old_date != new_date:
-            print("Auction has been refreshed")
-            new_auction_data['date'] = new_date
-
-        if len(new_auction_data) > 1:
-            user_choice = input("Some data in auction has been changed. Do you want to update database? (Y/N)")
-            while True:
-                if user_choice.lower() == "n":
-                    print("Database won't be updated")
-                    break
-                elif user_choice.lower() == "y":
-                    print("Database will be updated")
-                    db.update({'title': new_title, 'price': new_price, 'date': new_date}, where('link') == link)
-                    break
+        update_db(auction_data, search_result)
 
     else:
         db.insert(auction_data)
 
 
 def check_links():
-    pass
+    for record in db.all():
+        r = requests.get(record['link'])
 
 
 if __name__ == "__main__":
-    add_link()
+    while True:
+        start = int(input("1 - add link\n2 - check all links"))
+        match start:
+            case 1:
+                add_link()
+            case 2:
+                check_links()
